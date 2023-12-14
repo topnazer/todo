19207ed86ea25c2e91b6782b7d35b9770d130ade
topnazer <topnazer@gmail.com>
2023-12-14 22:52:07 +0800

    sheshh

--- todos/src/TodoA/index.jsx
+++ todos/src/TodoA/index.jsx
@@ -2,7 +2,6 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Theme } from './Theme/Theme';
 export { default as TaskList } from './TaskList/TaskList';
 export { default as Satistics } from './Satistics/Satistics';
-export { default as Task } from './Task/Task';
 export { default as TaskForm } from './TaskForm/TaskForm';
 
 
